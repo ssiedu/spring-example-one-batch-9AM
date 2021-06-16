475387d926f83c83f7970972cdd86dbe5e9320b3
manoj <manojs121@rediffmail.com>
2021-06-16 10:03:25 +0530

    160621

---
+++ src/main/java/com/ssi/Address.java
@@ -0,0 +1,10 @@
+package com.ssi;
+
+import lombok.Data;
+
+@Data
+public class Address {
+	private int hno;
+	private String street;
+	private String city;
+}
--- src/main/java/com/ssi/Info.java
+++ src/main/java/com/ssi/Info.java
@@ -2,14 +2,16 @@ package com.ssi;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Properties;
 import java.util.Set;
 
 public class Info {
 	private String companyName;
-	private String companyAddress;
+	private List<Address> companyAddresses;
 	private List<String> emails;
 	private Set<String> mobiles;
 	private Map<String,String> management;
+	private Properties branches;
 	
 	public List<String> getEmails() {
 		return emails;
@@ -23,11 +25,11 @@ public class Info {
 	public void setCompanyName(String companyName) {
 		this.companyName = companyName;
 	}
-	public String getCompanyAddress() {
-		return companyAddress;
+	public List<Address> getCompanyAddresses() {
+		return companyAddresses;
 	}
-	public void setCompanyAddress(String companyAddress) {
-		this.companyAddress = companyAddress;
+	public void setCompanyAddresses(List<Address> companyAddresses) {
+		this.companyAddresses = companyAddresses;
 	}
 	public Set<String> getMobiles() {
 		return mobiles;
@@ -41,11 +43,19 @@ public class Info {
 	public void setManagement(Map<String, String> management) {
 		this.management = management;
 	}
+	
+	public Properties getBranches() {
+		return branches;
+	}
+	public void setBranches(Properties branches) {
+		this.branches = branches;
+	}
 	@Override
 	public String toString() {
-		return "Info [companyName=" + companyName + ", companyAddress=" + companyAddress + ", emails=" + emails
-				+ ", mobiles=" + mobiles + ", management=" + management + "]";
+		return "Info [companyName=" + companyName + ", companyAddresses=" + companyAddresses + ", emails=" + emails
+				+ ", mobiles=" + mobiles + ", management=" + management + ", branches=" + branches + "]";
 	}
+
 	
 
 	
